85b8ea5d0510e773e2974fdcee6d0f703f395c46
ilhmakbrr <ilhamakbar1203@gmail.com>
2025-06-15 17:20:30 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/auth/login.dart';
